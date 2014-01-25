b04283773f21c618ceb61e7c468877333841a626
LoganKnecht@gmail.com <LoganKnecht@gmail.com@d425d03e-2285-fc3f-d1ba-bbbbdd2acd6e>
2014-01-25 21:36:38 +0000

    Fixed targeting for player moving. Going to work on logic for the player's overall actions now.

--- trunk/Assets/Scripts/Player/Player.cs
+++ trunk/Assets/Scripts/Player/Player.cs
@@ -7,15 +7,13 @@ public class Player : TargetPathingGameObject {
 //	// Use this for initialization
 //	public override void Start () {
 //	}
-//	// Update is called once per frame
-//	public override void Update () {
-//	}
-//	public void OnCollisionEnter(Collision collisionInfo) {
-//		Debug.Log("Collision detected");
-//		HouseItem houseItem = collisionInfo.gameObject.GetComponent<HouseItem>();
-//		if(houseItem != null) {
-//		}
-//	}
+
+	// Update is called once per frame
+	public override void Update () {
+		base.Update();
+		PerformPlayerArrivalLogic();
+	}
+
 	public void PerformPlayerArrivalLogic() {
 		if(this.gameObject.transform.position.x == targetPosition.x
 		   && this.gameObject.transform.position.y == targetPosition.y) {
@@ -26,6 +24,31 @@ public class Player : TargetPathingGameObject {
 	}
 
 	public override void SetTargetObjectAndTargetPosition(GameObject newTargetObject, Vector2 newTargetPosition) {
+		if(useWorldBoundings) {
+			//Left
+			float widthOffset = this.gameObject.transform.localScale.x/2;
+			float heightOffset = this.gameObject.transform.localScale.y/2;
+
+			//Left
+			if((newTargetPosition.x - widthOffset) < minWorldXBounding) {
+				newTargetPosition.x = (minWorldXBounding + widthOffset);
+			}
+			//Right
+			else if((newTargetPosition.x + widthOffset) > maxWorldXBounding) {
+				newTargetPosition.x = (maxWorldXBounding - widthOffset);
+			}
+
+			//Top
+			if((newTargetPosition.y + heightOffset) > minWorldYBounding) {
+				newTargetPosition.y = (minWorldYBounding - heightOffset);
+			}
+			//Bottom
+			else if((newTargetPosition.y - heightOffset) < maxWorldYBounding) {
+				newTargetPosition.y = (maxWorldYBounding + heightOffset);
+			}
+	
+		}
+
 		arrivedAtTargetPosition = false;
 		targetObject = newTargetObject;
 		targetPosition = newTargetPosition;
