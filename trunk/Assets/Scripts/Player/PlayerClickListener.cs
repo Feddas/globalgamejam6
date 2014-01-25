45ba35896e13651619ff89336fc2db2fd60cc338
LoganKnecht@gmail.com <LoganKnecht@gmail.com@d425d03e-2285-fc3f-d1ba-bbbbdd2acd6e>
2014-01-25 11:05:44 +0000

    Created the click listener class we discussed. However, there is an incredible over site that I need to talk to Shawn about. The on click detection just gets the input from the mouse, and that configures it so that the player gets the translated coordinates of what was clicked. However I don't think that the targetObject will be detectable without writing some code that parses the objects in the scene and says ok this is what you clicked on I think. Need to make it so that it detects the click, returns a game object that is relevant to logic, etc. Whether this is done on a per object basis of onclick, or whether it's done some how else.

---
+++ trunk/Assets/Scripts/Player/PlayerClickListener.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Click listener - This class assumes that it is attached to a game object that contains the script "Player"
+/// 	if it's not then this will break. This is tightly coupled behavior for the player.
+/// 
+/// There either needs to be a way to detect the object being clicked or to communicate in the code what object on the screen is clicked
+/// You can have it so each object manages their own on click logic, but if that's the case then the target object needs to be set maybe?
+/// Talk to Shawn more about it. Reason being is that I'm concerned the player listener won't correctly path to objects if it doesn't know what object
+/// is occurring on click. This is because this performs a general on click detection and translates that position to worldview. None of this performs
+/// per object on mouse down detection /endtireddoc'ing
+/// </summary>
+public class PlayerClickListener : MonoBehaviour {
+	
+	public Player playerReference = null;
+
+	// Use this for initialization
+	void Start () {
+		playerReference = this.gameObject.GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PerformClickListenerLogic();
+	}
+
+	void PerformClickListenerLogic() {
+		if(Input.GetMouseButtonDown(0)) {
+			Vector3 mousePosition = Input.mousePosition;
+			Vector3 worldPosition = Camera.main.camera.ScreenToWorldPoint(mousePosition);
+			//Debug.Log("Left Click Detected");
+			//Debug.Log("Mouse Screen Position: " + mousePosition);
+			//Debug.Log("Mouse World Position: " + worldPosition);
+
+			//TO DO trigger call for separate logic branches here for better code management in case it needs to be refined
+			//Maybe? Colliders should be attached to game objects, so maybe have it decide if it should path or do something special
+			//who knows
+			SetPlayerReferenceTargetObjectAndTargetPosition(null, new Vector2(worldPosition.x, worldPosition.y));
+		}
+	}
+
+	void SetPlayerReferenceTargetObjectAndTargetPosition(GameObject newTargetObject, Vector3 newTargetPosition) {
+		if(playerReference != null) {
+			playerReference.SetTargetOjbectAndTargetPosition(newTargetObject, newTargetPosition);
+		}
+	}
+}
---
+++ trunk/Assets/Scripts/Player/TargetPathingGameObject.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetPathingGameObject : MonoBehaviour {
+
+	public GameObject targetObject = null;
+	public Vector2 targetPosition = Vector2.zero;
+
+	public bool useWorldBoundings = false;
+	public float minWorldXBounding = 0;
+	public float maxWorldXBounding = 0;
+	public float minWorldYBounding = 0;
+	public float maxWorldYBounding = 0;
+
+	public float xMovementSpeed = 1;
+	public float yMovementSpeed = 1;
+
+	public float targetPositionXLockBuffer = 0;
+	public float targetPositionYLockBuffer = 0;
+
+	// Use this for initialization
+	public virtual void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+		//Debug.Log("Performing Movement Logic");
+		PerformMovementLogic();
+		//PerformTargetPositionLocking();
+	}
+
+	public void PerformMovementLogic() {
+		Vector2 nextMoveStepSize = CalculateNextMovementStep();
+		nextMoveStepSize = PerformTargetPositionLocking(nextMoveStepSize);
+		Vector3 nextWorldPosition = this.gameObject.transform.position + (new Vector3(nextMoveStepSize.x, nextMoveStepSize.y, this.gameObject.transform.position.z));
+
+		//Not super accurate but does the job
+		float widthOffset = (this.gameObject.transform.localScale.x/2);
+		float heightOffset = (this.gameObject.transform.localScale.y/2);
+		//Debug.Log("Current Position: " + this.gameObject.transform.position);
+		//Debug.Log("Next Position: " + nextWorldPosition);
+		//Debug.Log("Next Position With X Bound: " + (nextWorldPosition.x + this.gameObject.transform.localScale.x/2);
+		//Debug.Log(this.gameObject.transform.localScale.x/2);
+		if(useWorldBoundings) {
+			//Left X
+			if((nextWorldPosition.x - widthOffset) < minWorldXBounding) {
+				nextWorldPosition.x = (minWorldXBounding + widthOffset);
+			}
+			//Right X
+			else if((nextWorldPosition.x + widthOffset) > maxWorldXBounding) {
+				nextWorldPosition.x = (maxWorldXBounding -	 widthOffset);;
+			}
+
+			//Top Y
+			if((nextWorldPosition.y + heightOffset) > minWorldYBounding) {
+				nextWorldPosition.y = (minWorldYBounding - heightOffset);
+				//Debug.Log("Min World Y bounding reset");
+			}
+			//Bottom Y
+			else if((nextWorldPosition.y - heightOffset) < maxWorldYBounding) {
+				nextWorldPosition.y = (maxWorldYBounding + heightOffset);
+				//Debug.Log("Max World Y bounding reset");
+			}
+		}
+		
+		this.gameObject.transform.position = nextWorldPosition;
+	}
+
+	public Vector2 PerformTargetPositionLocking(Vector2 newPositionOffset) {
+		//x locker
+		//			Debug.Log("target x pos great: " + (targetPosition.x - targetPositionXLockBuffer));
+		//			Debug.Log("target x pos less: " + (targetPosition.x + targetPositionXLockBuffer));
+		//			Debug.Log("target x pos: " + targetPosition.x);
+		//			Debug.Log("next x pos: " + (this.gameObject.transform.position.x + newPositionOffset.x));
+		if((this.gameObject.transform.position.x + newPositionOffset.x) > (targetPosition.x - targetPositionXLockBuffer)
+		   && (this.gameObject.transform.position.x + newPositionOffset.x) < (targetPosition.x + targetPositionXLockBuffer)) {
+			//Debug.Log("setting x position to target");
+			this.gameObject.transform.position = new Vector3(targetPosition.x,
+			                                                 this.gameObject.transform.position.y,
+			                                                 this.gameObject.transform.position.z);
+			newPositionOffset.x = 0;
+		}
+		//			//y locker
+		if((this.gameObject.transform.position.y + newPositionOffset.y) > (targetPosition.y - targetPositionYLockBuffer)
+		   && (this.gameObject.transform.position.y + newPositionOffset.y) < (targetPosition.y + targetPositionYLockBuffer)) {
+			//Debug.Log("setting y position to target");
+			this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x,
+			                                                 targetPosition.y,
+			                                                 this.gameObject.transform.position.z);
+			newPositionOffset.y = 0;
+		} 
+		
+		return newPositionOffset;
+	}
+
+	public void SetTargetOjbectAndTargetPosition(GameObject newTargetObject, Vector2 newTargetPosition) {
+		targetObject = newTargetObject;
+		targetPosition = newTargetPosition;
+	}
+
+	public Vector2 CalculateNextMovementStep() {
+		Vector2 movementDistance = Vector2.zero;
+
+		if(this.gameObject.transform.position.x < targetPosition.x) {
+			movementDistance.x += xMovementSpeed;
+		}
+		else if(this.gameObject.transform.position.x > targetPosition.x) {
+			movementDistance.x -= xMovementSpeed;
+		}
+		if(this.gameObject.transform.position.y < targetPosition.y) {
+			movementDistance.y += yMovementSpeed;
+		}
+		else if(this.gameObject.transform.position.y > targetPosition.y) {
+			movementDistance.y -= yMovementSpeed;
+		}
+
+		return movementDistance;
+	}
+
+	public bool hasTargetObjectArrivedAtLocation() {
+		if(this.gameObject.transform.position.x == targetPosition.x
+		   && this.gameObject.transform.position.y == targetPosition.y) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
