a25ac87d0e3c3395204be7a8cf0fd770fd670a00
feddas00@gmail.com <feddas00@gmail.com@d425d03e-2285-fc3f-d1ba-bbbbdd2acd6e>
2014-01-25 22:30:03 +0000

    Singleton-ify-ed TextLibrary.  Breaking for lunch.

--- trunk/Assets/Scripts/SceneManagers/TextLibrary.cs
+++ trunk/Assets/Scripts/SceneManagers/TextLibrary.cs
@@ -4,23 +4,36 @@ using System.Collections.Generic;
 
 public class TextLibrary
 {
-	//TODO: singleton-ify this class
+	private static volatile TextLibrary _instance;
+	private static object _lock = new object();
 
+	public static TextLibrary Instance {
+		get {
+			if (_instance == null) {
+				lock(_lock) {
+					if (_instance == null) 
+						_instance = new TextLibrary();
+				}
+			}
+			return _instance;
+		}
+	}
+	
 	/// <summary> Text for all house items is sorted first by the item type then by observing the player state to index the List<string> </summary>
 	Dictionary<HouseItemType, List<string>> TextHouseItem;
-
+	
 	/// <summary> Various dialog for the flashback animations </summary>
 	Dictionary<Room, List<string>> TextFlashback;
-
+	
 	/// <summary> Dialogs trigged by a zone, such as by the front door</summary>
 	Dictionary<Room, List<string>> TextZone;
+	
+	//Stops the lock being created ahead of time if it's not necessary
+	static TextLibrary() {
+	}
 
-	public TextLibrary()
-	{
-		TextHouseItem = new Dictionary<HouseItemType, List<string>>();
-		TextHouseItem.Add(HouseItemType.AtticBoxes, new List<string>{
-			"What fancy boxes",
-			"Are those from the 1800's?"});
+	private TextLibrary() {
+		loadTextHouseItem();
 	}
 
 	public string GetTextFor(HouseItemType targetItem)
@@ -30,7 +43,7 @@ public class TextLibrary
 		switch (targetItem)
 		{
 		case HouseItemType.AtticBoxes:
-			itemState = 0;//TODO: observe player object to determine which itemState string to use
+			itemState = 0; //TODO: observe player object to determine which itemState string to use
 			break;
 
 		default:
@@ -38,5 +51,13 @@ public class TextLibrary
 		}
 
 		return TextHouseItem[targetItem][itemState];
+	}
+	
+	private void loadTextHouseItem()
+	{
+		TextHouseItem = new Dictionary<HouseItemType, List<string>>();
+		TextHouseItem.Add(HouseItemType.AtticBoxes, new List<string>{
+			"What fancy boxes",
+			"Are those from the 1800's?"});
 	}
 }
