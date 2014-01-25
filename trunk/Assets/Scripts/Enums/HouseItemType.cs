1426035c5bc6b470a36fa5f4c307d5acdeaf0c67
LoganKnecht@gmail.com <LoganKnecht@gmail.com@d425d03e-2285-fc3f-d1ba-bbbbdd2acd6e>
2014-01-25 18:55:18 +0000

    Added entry room scene and living room scene.

--- trunk/Assets/Scripts/Enums/HouseItemType.cs
+++ trunk/Assets/Scripts/Enums/HouseItemType.cs
@@ -2,11 +2,15 @@
 	//Please please please! Make sure that this is organized alphabetically?
 	//It's going to get big fast, except for None, that's at the top
 	None,
-	AtticDoor,
 	Car,
+	DoorAttic,
+	DoorFoyer,
+	DoorFront,
 	FirePoker,
 	Floor,
 	FrontDoor,
+	LivingRoomDoor,
+	Mirror,
 	PictureFrame,
 	Pillow1,
 	Pillow2,
