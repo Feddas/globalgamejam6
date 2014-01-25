cc6d34d569c93bf10c9eac7ec0ce217387f01fbc
LoganKnecht@gmail.com <LoganKnecht@gmail.com@d425d03e-2285-fc3f-d1ba-bbbbdd2acd6e>
2014-01-25 13:58:05 +0000

    Adding more placeholder assets and door trigger added to move to other scene. There is more to work on, but I have to catch the train/bus to seattle.

---
+++ trunk/Assets/Scripts/House/DoorSceneTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSceneTrigger : MonoBehaviour {
+	public string sceneToChangeTo = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+		Debug.Log("Door change detected.");
+		if(otherCollider.gameObject.GetComponent<Player>() != null) {
+			if(!sceneToChangeTo.Equals(string.Empty)) {
+				Application.LoadLevel(sceneToChangeTo);
+			}
+		}
+	}
+}
--- trunk/Assets/Scripts/Player/Player.cs
+++ trunk/Assets/Scripts/Player/Player.cs
@@ -2,10 +2,32 @@
 using System.Collections;
 
 public class Player : TargetPathingGameObject {
+	public bool arrivedAtTargetPosition = false;
+
 //	// Use this for initialization
 //	public override void Start () {
 //	}
 //	// Update is called once per frame
 //	public override void Update () {
 //	}
+//	public void OnCollisionEnter(Collision collisionInfo) {
+//		Debug.Log("Collision detected");
+//		HouseItem houseItem = collisionInfo.gameObject.GetComponent<HouseItem>();
+//		if(houseItem != null) {
+//		}
+//	}
+	public void PerformPlayerArrivalLogic() {
+		if(this.gameObject.transform.position.x == targetPosition.x
+		   && this.gameObject.transform.position.y == targetPosition.y) {
+			if(!arrivedAtTargetPosition) {
+				arrivedAtTargetPosition = true;
+			}
+		}
+	}
+
+	public override void SetTargetOjbectAndTargetPosition(GameObject newTargetObject, Vector2 newTargetPosition) {
+		arrivedAtTargetPosition = false;
+		targetObject = newTargetObject;
+		targetPosition = newTargetPosition;
+	}
 }
