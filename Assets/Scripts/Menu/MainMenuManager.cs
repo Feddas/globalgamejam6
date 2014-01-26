26aaf887b8aad813e610ad09b8936ce1ba811429
Logan Knecht <loganknecht@users.noreply.github.com>
2014-01-26 15:42:58 +0000

    Configured main menu for the correct direction of triggering the game start.

--- Assets/Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -5,22 +5,33 @@ public class MainMenuManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//Player.GetComponent<Player>().SetPlayerCurrentState(PlayerState.InCinematic);
-
-		//if(Player.Instance == false) {
-		//}
-		Player.Instance.currentState = PlayerState.InCinematic;
-		GameObject cinematicToPerformGameObject = new GameObject("Cinematic");
-		cinematicToPerformGameObject.AddComponent<StartGameCinematic>();
-		Player.Instance.cinematicToPerform = cinematicToPerformGameObject;
-		//Keeps it alive so the player's cinematic isn't destroyed
-		DontDestroyOnLoad(cinematicToPerformGameObject);
-
-		//Debug.Log(Player.Instance.currentState);
-		Application.LoadLevel("FrontHouse");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	}
+
+	void OnGUI() {
+		GUIStyle gameTitleStyle = new GUIStyle();
+		gameTitleStyle.fontSize = 64;
+		gameTitleStyle.normal.textColor = Color.white;
+		GUI.Label(new Rect(200, 150, 200, 200), "Broken Home", gameTitleStyle);
+
+		if(GUI.Button(new Rect(325, 300, 150, 75), "Start")) {
+			Player.Instance.currentState = PlayerState.InCinematic;
+			GameObject cinematicToPerformGameObject = new GameObject("Cinematic");
+			cinematicToPerformGameObject.AddComponent<StartGameCinematic>();
+			Player.Instance.cinematicToPerform = cinematicToPerformGameObject;
+			Player.Instance.transform.position = new Vector3(3.05f, -3, Player.Instance.transform.position.z);
+			Player.Instance.targetPosition = new Vector2(3.05f, -3);
+			//Keeps it alive so the player's cinematic isn't destroyed
+			DontDestroyOnLoad(cinematicToPerformGameObject);
+			
+			//Debug.Log(Player.Instance.currentState);
+			Application.LoadLevel("FrontHouse");
+		}
+		if(GUI.Button(new Rect(325, 375, 150, 75), "Credits")) {
+			//Application.LoadLevel("FrontHouse");
+		}
 	}
 }
