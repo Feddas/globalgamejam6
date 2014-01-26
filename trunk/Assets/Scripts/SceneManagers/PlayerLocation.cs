27d8f98af302cb87fe8a6e87d54a570e829f2ec4
feddas00@gmail.com <feddas00@gmail.com@d425d03e-2285-fc3f-d1ba-bbbbdd2acd6e>
2014-01-26 05:44:11 +0000

    Backgrounds for Bedroom and Masterbed.

--- trunk/Assets/Scripts/SceneManagers/PlayerLocation.cs
+++ trunk/Assets/Scripts/SceneManagers/PlayerLocation.cs
@@ -3,19 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 
 /// <summary> Spawn location is the coords for where the player started after entering this scene </summary>
-public class SpawnLocation
+public class PlayerLocation
 {
 	Dictionary<Tuple<Room, Room>, Vector2> spawnLocations = new Dictionary<Tuple<Room, Room>, Vector2>();
+	Dictionary<Tuple<Room, Room>, Vector2> maxBounds = new Dictionary<Tuple<Room, Room>, Vector2>();
 
-	private static volatile SpawnLocation _instance;
+	private static volatile PlayerLocation _instance;
 	private static object _lock = new object();
 	
-	public static SpawnLocation Instance {
+	public static PlayerLocation Instance {
 		get {
 			if (_instance == null) {
 				lock(_lock) {
 					if (_instance == null) 
-						_instance = new SpawnLocation();
+						_instance = new PlayerLocation();
 				}
 			}
 			return _instance;
@@ -23,11 +24,13 @@ public class SpawnLocation
 	}
 	
 	//Stops the lock being created ahead of time if it's not necessary
-	static SpawnLocation() {
+	static PlayerLocation() {
 	}
 	
-	private SpawnLocation() {
+	private PlayerLocation() {
 		addSpawnLocation(Room.Hallway, Room.Attic, -2.5f, -3f);
+		addSpawnLocation(Room.Hallway, Room.Masterbed, -4.4f, -3.6f);
+		addSpawnLocation(Room.Hallway, Room.Bedroom, 6.2f, -3f);
 	}
 
 	private void addSpawnLocation(Room fromRoom, Room toRoom, float x, float y)
@@ -43,5 +46,34 @@ public class SpawnLocation
 			return spawnLocations[key];
 		else
 			return new Vector2(0f, -3.5f);
+	}
+
+	public Vector2 GetMaxBoundsInScene(Room targetRoom)
+	{
+		Vector2 result = new Vector2(3.6f, -4.9f);
+
+		switch (targetRoom)
+		{
+		case Room.Attic:
+		case Room.Foyer:
+		case Room.Hallway:
+		case Room.LivingRoom:
+			result.x = 6.5f;
+			break;
+			
+		case Room.Bedroom:
+			result.x = 8.6f;
+			break;
+		case Room.Masterbed:
+			result.x = 6.8f;
+			break;
+			
+			//everything else
+		default:
+			result.x = 3.6f;
+			break;
+		}
+
+		return result;
 	}
 }
