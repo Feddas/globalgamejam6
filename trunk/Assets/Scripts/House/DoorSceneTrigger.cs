b0a35cff0a263525f39f124731a5f0dbe551210a
LoganKnecht@gmail.com <LoganKnecht@gmail.com@d425d03e-2285-fc3f-d1ba-bbbbdd2acd6e>
2014-01-26 22:44:59 +0000

    Fixed several scenes for a better "feel".

--- trunk/Assets/Scripts/House/DoorSceneTrigger.cs
+++ trunk/Assets/Scripts/House/DoorSceneTrigger.cs
@@ -18,6 +18,50 @@ public class DoorSceneTrigger : MonoBehaviour {
 		if(otherCollider.gameObject.GetComponent<Player>() != null) {
 			if(!sceneToChangeTo.Equals(string.Empty)) {
 				Player.Instance.UpdatePlayerLocation(currentScene, sceneToChangeTo);
+				Player.Instance.currentState = PlayerState.Standing;
+				//USE THIS TO CONFIGURE THE PLAYERS BOUNDING FOR EACH SCENE BEING ENTERED
+				switch(sceneToChangeTo) {
+					case(Room.None):
+					break;
+					case(Room.Attic):
+					break;
+					case(Room.Bedroom):
+					break;
+					case(Room.Foyer):
+						//Player.Instance.minWorldXBounding = -0.9f;
+						//Player.Instance.maxWorldXBounding = -0.9f;
+						Player.Instance.minWorldYBounding = -2f;
+						Player.Instance.maxWorldYBounding = -4.9f;
+					break;
+					case(Room.FrontHouse):
+					break;
+					case(Room.Hallway):
+						//Player.Instance.minWorldXBounding = -0.9f;
+						//Player.Instance.maxWorldXBounding = -0.9f;
+						Player.Instance.minWorldYBounding = -0.9f;
+						Player.Instance.maxWorldYBounding = -3.9f;
+						if(currentScene == Room.Attic) {
+							Player.Instance.gameObject.transform.position = new Vector3(-0.1140994f, -2.71987f, Player.Instance.gameObject.transform.position.z);
+							Player.Instance.targetPosition = new Vector2(-0.1140994f, -2.71987f);
+						}
+						else if(currentScene == Room.Bedroom) {
+							Player.Instance.gameObject.transform.position = new Vector3(-0.1140994f, -2.71987f, Player.Instance.gameObject.transform.position.z);
+							Player.Instance.targetPosition = new Vector2(-0.1140994f, -2.71987f);
+						}
+						else if(currentScene == Room.Masterbed) {
+							Player.Instance.gameObject.transform.position = new Vector3(-0.1140994f, -2.71987f, Player.Instance.gameObject.transform.position.z);
+							Player.Instance.targetPosition = new Vector2(-0.1140994f, -2.71987f);
+						}
+						else if(currentScene == Room.Foyer) {
+							Player.Instance.gameObject.transform.position = new Vector3(-0.1140994f, -2.71987f, Player.Instance.gameObject.transform.position.z);
+							Player.Instance.targetPosition = new Vector2(-0.1140994f, -2.71987f);
+						}
+					break;
+					case(Room.LivingRoom):
+					break;
+					case(Room.Masterbed):
+					break;
+				}
 				if(audio)
 					audio.Play();
 				Application.LoadLevel(sceneToChangeTo.ToString());
