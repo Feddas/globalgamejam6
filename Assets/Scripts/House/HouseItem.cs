661fbdf569f6122e9377f3bb0e68a06ec32fd500
Shawn Featherly <Feddas@users.noreply.github.com>
2014-01-26 22:35:53 +0000

    Text display attempt

--- Assets/Scripts/House/HouseItem.cs
+++ Assets/Scripts/House/HouseItem.cs
@@ -27,6 +27,7 @@ public class HouseItem : HouseBaseObject {
 		if (debugText != TextLibrary.CompletedDialog)
 			Debug.Log(debugText);
 		Debug.Log(TextLibrary.Instance.GetTextFor(type));
+		showText(debugText);
 
 		Vector3 mousePosition = Input.mousePosition;
 		Vector3 worldPosition = Camera.main.camera.ScreenToWorldPoint(mousePosition);
@@ -34,6 +35,26 @@ public class HouseItem : HouseBaseObject {
 		//Debug.Log("calling player set target from house item");
 		if(playerReference.currentState != PlayerState.InCinematic) {
 			playerReference.SetTargetObjectAndTargetPosition(this.gameObject, worldPosition);
+		}
+	}
+
+	bool newText;
+	string textBoxText = "this is a test of the broad";
+	private void showText(string text)
+	{
+		newText = true;
+		this.textBoxText = text;
+	}
+
+	public void OnGUI() {
+		if(newText) {
+			var screenHeight = StartGameCinematic.textboxWidth;
+			var textboxHeight = StartGameCinematic.textboxHeight;
+			var textboxWidth = StartGameCinematic.textboxWidth;
+			if(GUI.Button(new Rect(735, (screenHeight - textboxHeight) - 64, 64, 64), "Next")) {
+				newText = false;
+			}
+			GUI.TextArea(new Rect(0, screenHeight - textboxHeight, textboxWidth, textboxHeight), textBoxText);
 		}
 	}
 }
--- Assets/Scripts/SceneManagers/TextLibrary.cs
+++ Assets/Scripts/SceneManagers/TextLibrary.cs
@@ -7,7 +7,7 @@ public class TextLibrary
 	public const string CompletedDialog = "<CompletedDialog>";
 	private static volatile TextLibrary _instance;
 	private static object _lock = new object();
-
+	
 	public static TextLibrary Instance {
 		get {
 			if (_instance == null) {
@@ -22,26 +22,26 @@ public class TextLibrary
 	
 	/// <summary> Text for all house items is sorted first by the item type then by observing the player state to index the List<string> </summary>
 	Dictionary<HouseItemType, List<string>> TextHouseItem;
-		
+	
 	/// <summary> Dialogs triggered by a flashback or entering a room (such as by the front door)</summary>
 	Dictionary<Room, List<string>> TextRoom;
 	
 	//Stops the lock being created ahead of time if it's not necessary
 	static TextLibrary() {
 	}
-
+	
 	private TextLibrary() {
 		loadTextHouseItem();
 		loadTextRoom();
 	}
-
+	
 	public string GetTextFor(HouseItemType targetItem)
 	{
 		int itemState = 0;
-
+		
 		if (TextHouseItem.ContainsKey(targetItem) == false)
 			return null;
-
+		
 		switch (targetItem)
 		{
 		case HouseItemType.AtticRockingHorse:
@@ -51,42 +51,81 @@ public class TextLibrary
 		case HouseItemType.AtticBoxes:
 			itemState = Random.Range(0, 2);
 			break;
-
+			
 		default:
 			break;
 		}
-
+		
 		if (itemState > TextHouseItem[targetItem].Count - 1)
 			return CompletedDialog;
 		else
 			return TextHouseItem[targetItem][itemState];
 	}
-
+	
 	public string GetTextFor(Room targetRoom)
 	{
 		if (TextRoom.ContainsKey(targetRoom) == false)
 			return null;
-
+		
 		return TextRoom[targetRoom][0];
 	}
 	
 	private void loadTextHouseItem()
 	{
 		TextHouseItem = new Dictionary<HouseItemType, List<string>>();
-		TextHouseItem.Add(HouseItemType.AtticRockingHorse, new List<string>{
-			"I wonder why this old toy was moving",
-			"Opps, knocked the toy bunny over."});
 		TextHouseItem.Add(HouseItemType.AtticBoxes, new List<string>{
-			"What fancy boxes",
-			"Are those from the 1800's?"});
+			"These are filled with Liz’s old stuff…"});
+		TextHouseItem.Add(HouseItemType.AtticRockingHorse, new List<string>{
+			"The attic used to be empty. This must be where Dad put all of the old things after Mom died."});
+		TextHouseItem.Add(HouseItemType.BedroomBed, new List<string>{
+			"My old bed… it’s just as I left it. I think I’ll lay down for a moment…"});
+		TextHouseItem.Add(HouseItemType.BedroomPainting, new List<string>{//Poster
+			"I can’t believe I listened to them…"});
+		TextHouseItem.Add(HouseItemType.FrontCar, new List<string>{
+			"I did what I came for. Time to put this place, and the past, in the mirror…"});
+		TextHouseItem.Add(HouseItemType.LivingroomCodex, new List<string>{//without all pieces
+			"This looks like it would fit in the puzzle box. I guess these things are just lying about. I wonder how many more there are."});
+		TextHouseItem.Add(HouseItemType.LivingroomFirePoker, new List<string>{
+			"This place is even creepier than I remember. Maybe I’ll keep this with me…just in case."});
+		TextHouseItem.Add(HouseItemType.LivingroomFireplaceMantle, new List<string>{
+			"Here you go Dad. You have the place all to yourself, just like you wanted."});
+		TextHouseItem.Add(HouseItemType.LivingroomPaintingFireplace, new List<string>{
+			"I don’t remember this being here… it looks like there’s a note tucked behind the canvas."});
+		TextHouseItem.Add(HouseItemType.LivingroomPaintingsCreepy, new List<string>{
+			"The original home owners. Mom said these paintings had charm, but I always felt like they were staring at me."});
+		TextHouseItem.Add(HouseItemType.FoyerChair, new List<string>{
+			"I always hid behind this while playing with Liz…"});
+		TextHouseItem.Add(HouseItemType.FoyerMirror, new List<string>{//without mirror shard
+			"The mirror… I remember…"});
+		TextHouseItem.Add(HouseItemType.DoorAttic, new List<string>{//without FirePoker
+			"None of us were ever able to open the attic once the chord broke. We always used something to reach up there…"});
+		TextHouseItem.Add(HouseItemType.HallDoorLockedBathroom, new List<string>{
+			"It's just the bathroom."});
+		TextHouseItem.Add(HouseItemType.HallFloorboard, new List<string>{
+			"Wasn’t Dad supposed to fix this? It looks loose…"});
+		TextHouseItem.Add(HouseItemType.MasterbedMirrorShard, new List<string>{
+			"This looks like it would fit somewhere…"});
 	}
 	
 	private void loadTextRoom()
 	{
 		TextRoom = new Dictionary<Room, List<string>>();
 		TextRoom.Add(Room.FrontHouse, new List<string>{
-			"Here we are, my dad's place.",
-			"He was a jerk, but I've got to prove it to myself that I'm better.",
-			"At his house, with his ashes in this urn I should have his request to have them placed on the fireplace mantle in no time."});
+			"It’s been so long since I’ve been home.",
+			"I hardly remember the place, but Dad wanted his ashes placed over the mantle. I’ll just put them there and leave.",
+			"Strange to have an unfinished inscription on the urn though. All it says is, YOU ARE."});
+		TextRoom.Add(Room.Attic, new List<string>{
+		});
+		TextRoom.Add(Room.Bedroom, new List<string>{
+		});
+		TextRoom.Add(Room.Foyer, new List<string>{
+		});
+		TextRoom.Add(Room.Hallway, new List<string>{
+		});
+		TextRoom.Add(Room.LivingRoom, new List<string>{
+		});
+		TextRoom.Add(Room.Masterbed, new List<string>{
+		});
+		
 	}
 }
