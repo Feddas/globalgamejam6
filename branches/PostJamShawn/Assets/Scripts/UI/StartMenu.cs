3e1e57401affe26571ed6a40c40b323668954a1e
feddas00@gmail.com <feddas00@gmail.com@d425d03e-2285-fc3f-d1ba-bbbbdd2acd6e>
2014-02-06 12:04:38 +0000

    initial StartMenu

---
+++ branches/PostJamShawn/Assets/Scripts/UI/StartMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour
+{
+	public GameObject FrontDoor;
+
+	void Start() { }
+	void Update() { }
+
+	public void OnClick( dfControl control, dfMouseEventArgs mouseEvent )
+	{
+		if (control.name == "Start")
+		{
+			State.Instance.Completed = Completion.Start;
+			FrontDoor.SetActive(true);
+			var startMenu = this.GetComponent<dfPanel>();
+			startMenu.IsVisible = false;
+		}
+		else if (control.name == "Trailer")
+		{
+			//May want to try richtext hyperlink http://www.daikonforge.com/dfgui/forums/topic/rich-text-anchors-as-buttons/
+			Application.OpenURL("https://drive.google.com/file/d/0B4RQwHQLBksVTlR6d0JOWVBZa2c/edit?usp=sharing");
+		}
+		else if (control.name == "Exit")
+		{
+			Application.Quit();
+		}
+	}
+}
